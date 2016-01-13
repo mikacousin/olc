d7572bf71a23ab9b420000e1bcb64341fbd0b62f
mika <mika.cousin@gmail.com>
2016-01-13 08:02:02 +0100

    Default size for Sequential Window

--- src/sequentialwindow.py
+++ src/sequentialwindow.py
@@ -9,6 +9,7 @@ class SequentialWindow(Gtk.Window):
         self.seq = seq
 
         Gtk.Window.__init__(self, title="Sequential")
+        self.set_default_size(820, 1000)
         self.set_border_width(10)
 
         position = self.seq.position
