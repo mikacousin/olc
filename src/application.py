9337d4defb97fe4c5b705b956e3461a682b7a11e
mika <mika.cousin@gmail.com>
2016-05-23 14:14:57 +0200

    Save As if no file open

--- src/application.py
+++ src/application.py
@@ -270,7 +270,13 @@ class Application(Gtk.Application):
         dialog.destroy()
 
     def _save(self, action, parameter):
-        self.ascii.save()
+        try:
+            self.ascii.save()
+        except:
+            self._saveas(None, None)
+
+    def _saveas(self, action, parameter):
+        print("Save As")
 
     def _patch(self, action, parameter):
         self.patchwindow = PatchWindow(self.patch, self.dmx, self.window)
