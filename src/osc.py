0c04a9caf0d88753362f2a80f5945335f246a0df
mika <mika.cousin@gmail.com>
2016-12-20 20:07:19 +0100

    Add DLuz support

--- src/osc.py
+++ src/osc.py
@@ -16,11 +16,15 @@ class OscClient(object):
             print(str(err))
             sys.exit()
 
-    def send(self, path, arg):
-        liblo.send(self.target, path, arg)
+    def send(self, path, *arg):
+        liblo.send(self.target, path, *arg)
 
 class OscServer(liblo.ServerThread):
     def __init__(self, window):
+
+        self.app = Gio.Application.get_default()
+        self.percent_view = Gio.Application.get_default().settings.get_boolean('percent')
+
         # Main Window
         self.window = window
         # Port to listen data from the client
@@ -58,6 +62,25 @@ class OscServer(liblo.ServerThread):
         self.add_method('/pad/moinspourcent', None, self.padminuspourcent_cb) # - %
         self.add_method('/pad/clear', None, self.padclear_cb) # Clear
 
+        # For DiLuz
+        self.add_method('/sub/launch', None, self.sub_launch_cb) # Open Masters page
+        self.add_method('/subStick/flash', 'ii', self.sub_flash_cb) # Flash Master (master number, level)
+        self.add_method('/subStick/level', 'ii', self.sub_level_cb) # Master level (master number, level)
+
+        # For TouchOSC
+        self.add_method('/sub/1/level', 'f', self.sub1_level_cb) # Master 1 (float entre 0 et 255)
+        self.add_method('/sub/2/level', 'f', self.sub2_level_cb) # Master 2 (float entre 0 et 255)
+        self.add_method('/sub/3/level', 'f', self.sub3_level_cb) # Master 3 (float entre 0 et 255)
+        self.add_method('/sub/4/level', 'f', self.sub4_level_cb) # Master 4 (float entre 0 et 255)
+        self.add_method('/sub/5/level', 'f', self.sub5_level_cb) # Master 5 (float entre 0 et 255)
+        self.add_method('/sub/6/level', 'f', self.sub6_level_cb) # Master 6 (float entre 0 et 255)
+        self.add_method('/sub/7/level', 'f', self.sub7_level_cb) # Master 7 (float entre 0 et 255)
+        self.add_method('/sub/8/level', 'f', self.sub8_level_cb) # Master 8 (float entre 0 et 255)
+        self.add_method('/sub/9/level', 'f', self.sub9_level_cb) # Master 9 (float entre 0 et 255)
+        self.add_method('/sub/10/level', 'f', self.sub10_level_cb) # Master 10 (float entre 0 et 255)
+        self.add_method('/sub/11/level', 'f', self.sub11_level_cb) # Master 1A (float entre 0 et 255)
+        self.add_method('/sub/12/level', 'f', self.sub12_level_cb) # Master 12 (float entre 0 et 255)
+
         #TODO
         self.add_method('/pad/enter', None, self.fallback) # Enter
         self.add_method('/pad/blackout', None, self.fallback) # Blackout (1 ou 0)
@@ -69,7 +92,6 @@ class OscServer(liblo.ServerThread):
         self.add_method('/seq/fadeX1', None, self.fallback) # (float entre 0 et 255)
         self.add_method('/seq/fadeX2', None, self.fallback) # (float entre 0 et 255)
 
-        self.add_method('/sub/1/level', None, self.fallback) # Master 1 (float entre 0 et 255)
         self.add_method('/sub/2/level', None, self.fallback) # Master 2 (float entre 0 et 255)
 
         # Register a fallback for unhandled messages
@@ -259,3 +281,72 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keypress_Escape()
                 self.client.send('/pad/saisieText', '')
+
+    def sub_launch_cb(self, path, args, types):
+        """ Launch Sub page """
+        for i in range(10):
+            self.client.send('/subStick/text', ('i', i+1), ('s', self.app.masters[i].text))
+
+    def sub_flash_cb(self, path, args, types):
+        """ Flash Master """
+        flash, level = args
+        if self.percent_view:
+            lvl = int((level / 255) * 100)
+        self.app.win_masters.scale[flash-1].set_value(lvl)
+        self.client.send('/subStick/level', ('i', flash), ('i', level))
+
+    def sub_level_cb(self, path, args, types):
+        """ Master Level """
+        flash, level = args
+        if self.percent_view:
+            lvl = int((level / 255) * 100)
+        self.app.win_masters.scale[flash-1].set_value(lvl)
+        self.client.send('/subStick/level', ('i', flash), ('i', level))
+
+    def sub1_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[0].set_value(int(level[0]))
+
+    def sub2_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[1].set_value(int(level[0]))
+
+    def sub3_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[2].set_value(int(level[0]))
+
+    def sub4_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[3].set_value(int(level[0]))
+
+    def sub5_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[4].set_value(int(level[0]))
+
+    def sub6_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[5].set_value(int(level[0]))
+
+    def sub7_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[6].set_value(int(level[0]))
+
+    def sub8_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[7].set_value(int(level[0]))
+
+    def sub9_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[8].set_value(int(level[0]))
+
+    def sub10_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[9].set_value(int(level[0]))
+
+    def sub11_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[10].set_value(int(level[0]))
+
+    def sub12_level_cb(self, path, args, types):
+        level = args
+        self.app.win_masters.scale[11].set_value(int(level[0]))
