8c3e31f58539646fa922bd3d2914c3bb0b75f053
mika <mika.cousin@gmail.com>
2017-01-23 21:16:49 +0100

    Fix on_click GroupWidget

--- src/customwidgets.py
+++ src/customwidgets.py
@@ -165,7 +165,7 @@ class GroupWidget(Gtk.Widget):
             self.clicked = True
         self.queue_draw()
         #self.wingrps.grp_flowbox1.invalidate_filter()
-        Gio.Application.get_default().tab.flowbox1.invalidate_filter()
+        Gio.Application.get_default().group_tab.flowbox1.invalidate_filter()
 
     def do_draw(self, cr):
         # paint background
