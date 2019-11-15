fd1461d0df856ff2247a22433bf170440876f9e6
mikacousin <mika.cousin@gmail.com>
2019-11-15 22:27:33 +0100

    Fader moving in Virtual Console

--- src/widgets_fader.py
+++ src/widgets_fader.py
@@ -25,6 +25,7 @@ class FaderWidget(Gtk.Widget):
         self.pressed = False
         self.value = 0
         self.inverted = False
+        self.on_fader = False
 
         self.text = text
 
@@ -33,15 +34,38 @@ class FaderWidget(Gtk.Widget):
         self.connect('button-press-event', self.on_press)
         self.add_events(Gdk.EventMask.BUTTON_RELEASE_MASK)
         self.connect('button-release-event', self.on_release)
+        self.add_events(Gdk.EventMask.POINTER_MOTION_MASK)
+        self.connect('motion-notify-event', self.on_motion)
 
     def on_press(self, tgt, ev):
         self.pressed = True
         self.queue_draw()
 
+        if self.inverted:
+            h = (((self.height - 20) / 255) * self.value)
+        else:
+            h = self.height - 20 - (((self.height - 20) / 255) * self.value)
+
+        if ev.y > h  and ev.y < h + 20:
+            self.on_fader = True
+
     def on_release(self, tgt, ev):
         self.pressed = False
         self.queue_draw()
         self.emit('clicked')
+
+    def on_motion(self, tgt, ev):
+        if self.pressed and self.on_fader and not self.app.midi.midi_learn:
+            if self.inverted:
+                val = ((self.height - 20) / 360) * ev.y
+            else:
+                val = self.height - 20 - (((self.height - 20) / 360) * ev.y)
+            if val < 0:
+                val = 0
+            elif val > 255:
+                val = 255
+            self.value = val
+            self.queue_draw()
 
     def do_draw(self, cr):
         # Draw vertical box
@@ -56,9 +80,15 @@ class FaderWidget(Gtk.Widget):
         else:
             h = self.height - 20 - (((self.height - 20) / 255) * self.value)
         if self.app.midi.midi_learn == self.text:
-            cr.set_source_rgb(0.3, 0.2, 0.2)
+            if self.pressed:
+                cr.set_source_rgb(0.2, 0.1, 0.1)
+            else:
+                cr.set_source_rgb(0.3, 0.2, 0.2)
         else:
-            cr.set_source_rgb(0.2, 0.2, 0.2)
+            if self.pressed:
+                cr.set_source_rgb(0.1, 0.1, 0.1)
+            else:
+                cr.set_source_rgb(0.2, 0.2, 0.2)
         area = (1, self.width - 2, h, h + 20)
         self.rounded_rectangle_fill(cr, area, self.radius)
         cr.set_source_rgb(0.1, 0.1, 0.1)
