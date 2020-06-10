ab4a35e30b7d2c3404ab7eebd7af092b823e6d72
mikacousin <mika.cousin@gmail.com>
2020-06-10 12:35:38 +0200

    Code cleanup

--- src/osc.py
+++ src/osc.py
@@ -1,7 +1,7 @@
+import sys
 import liblo
 from gi.repository import Gio
 
-from olc.window import Window
 
 class OscClient(object):
     def __init__(self):
@@ -12,12 +12,13 @@ class OscClient(object):
 
         try:
             self.target = liblo.Address(self.host, self.port)
-        except (liblo.AddressError, err):
-            print(str(err))
+        except liblo.AddressError as err:
+            print(err)
             sys.exit()
 
     def send(self, path, *arg):
         liblo.send(self.target, path, *arg)
+
 
 class OscServer(liblo.ServerThread):
     def __init__(self, window):
@@ -37,9 +38,9 @@ class OscServer(liblo.ServerThread):
         self.client = OscClient()
 
         # Add methods (strings the server will respond)
-        self.add_method('/seq/go', 'i', self.seqgo_cb) # Go
-        self.add_method('/seq/plus', 'i', self.seqplus_cb) # Seq +
-        self.add_method('/seq/moins', 'i', self.seqless_cb) # Seq -
+        self.add_method('/seq/go', 'i', self.seqgo_cb)           # Go
+        self.add_method('/seq/plus', 'i', self.seqplus_cb)       # Seq +
+        self.add_method('/seq/moins', 'i', self.seqless_cb)      # Seq -
         self.add_method('/pad/1', None, self.pad1_cb)
         self.add_method('/pad/2', None, self.pad2_cb)
         self.add_method('/pad/3', None, self.pad3_cb)
@@ -51,48 +52,63 @@ class OscServer(liblo.ServerThread):
         self.add_method('/pad/9', None, self.pad9_cb)
         self.add_method('/pad/0', None, self.pad0_cb)
         self.add_method('/pad/dot', None, self.paddot_cb)
-        self.add_method('/pad/channel', None, self.padchannel_cb)
+        self.add_method('/pad/channel', None, self.padchannel_cb)  # Channel
         self.add_method('/pad/all', None, self.padall_cb)
-        self.add_method('/pad/level', None, self.padlevel_cb) # Level
-        self.add_method('/pad/ff', None, self.padfull_cb) # Full
-        self.add_method('/pad/thru', None, self.padthru_cb) # Thru
-        self.add_method('/pad/plus', None, self.padplus_cb) # +
-        self.add_method('/pad/moins', None, self.padminus_cb) # -
-        self.add_method('/pad/pluspourcent', None, self.padpluspourcent_cb) # + %
-        self.add_method('/pad/moinspourcent', None, self.padminuspourcent_cb) # - %
-        self.add_method('/pad/clear', None, self.padclear_cb) # Clear
+        self.add_method('/pad/level', None, self.padlevel_cb)      # Level
+        self.add_method('/pad/ff', None, self.padfull_cb)          # Full
+        self.add_method('/pad/thru', None, self.padthru_cb)        # Thru
+        self.add_method('/pad/plus', None, self.padplus_cb)        # +
+        self.add_method('/pad/moins', None, self.padminus_cb)      # -
+        # + %
+        self.add_method('/pad/pluspourcent', None, self.padpluspourcent_cb)
+        # - %
+        self.add_method('/pad/moinspourcent', None, self.padminuspourcent_cb)
+        self.add_method('/pad/clear', None, self.padclear_cb)      # Clear
 
-        # For DiLuz
-        self.add_method('/sub/launch', None, self.sub_launch_cb) # Open Masters page
-        self.add_method('/subStick/flash', 'ii', self.sub_flash_cb) # Flash Master (master number, level)
-        self.add_method('/subStick/level', 'ii', self.sub_level_cb) # Master level (master number, level)
+        # For DiLuz :
+        # Open Masters page
+        self.add_method('/sub/launch', None, self.sub_launch_cb)
+        # Flash Master (master number, level)
+        self.add_method('/subStick/flash', 'ii', self.sub_flash_cb)
+        # Master level (master number, level)
+        self.add_method('/subStick/level', 'ii', self.sub_level_cb)
 
-        # For TouchOSC
-        self.add_method('/sub/1/level', 'f', self.sub1_level_cb) # Master 1 (float entre 0 et 255)
-        self.add_method('/sub/2/level', 'f', self.sub2_level_cb) # Master 2 (float entre 0 et 255)
-        self.add_method('/sub/3/level', 'f', self.sub3_level_cb) # Master 3 (float entre 0 et 255)
-        self.add_method('/sub/4/level', 'f', self.sub4_level_cb) # Master 4 (float entre 0 et 255)
-        self.add_method('/sub/5/level', 'f', self.sub5_level_cb) # Master 5 (float entre 0 et 255)
-        self.add_method('/sub/6/level', 'f', self.sub6_level_cb) # Master 6 (float entre 0 et 255)
-        self.add_method('/sub/7/level', 'f', self.sub7_level_cb) # Master 7 (float entre 0 et 255)
-        self.add_method('/sub/8/level', 'f', self.sub8_level_cb) # Master 8 (float entre 0 et 255)
-        self.add_method('/sub/9/level', 'f', self.sub9_level_cb) # Master 9 (float entre 0 et 255)
-        self.add_method('/sub/10/level', 'f', self.sub10_level_cb) # Master 10 (float entre 0 et 255)
-        self.add_method('/sub/11/level', 'f', self.sub11_level_cb) # Master 1A (float entre 0 et 255)
-        self.add_method('/sub/12/level', 'f', self.sub12_level_cb) # Master 12 (float entre 0 et 255)
+        # For TouchOSC :
+        # Master 1 (float entre 0 et 255)
+        self.add_method('/sub/1/level', 'f', self.sub1_level_cb)
+        # Master 2 (float entre 0 et 255)
+        self.add_method('/sub/2/level', 'f', self.sub2_level_cb)
+        # Master 3 (float entre 0 et 255)
+        self.add_method('/sub/3/level', 'f', self.sub3_level_cb)
+        # Master 4 (float entre 0 et 255)
+        self.add_method('/sub/4/level', 'f', self.sub4_level_cb)
+        # Master 5 (float entre 0 et 255)
+        self.add_method('/sub/5/level', 'f', self.sub5_level_cb)
+        # Master 6 (float entre 0 et 255)
+        self.add_method('/sub/6/level', 'f', self.sub6_level_cb)
+        # Master 7 (float entre 0 et 255)
+        self.add_method('/sub/7/level', 'f', self.sub7_level_cb)
+        # Master 8 (float entre 0 et 255)
+        self.add_method('/sub/8/level', 'f', self.sub8_level_cb)
+        # Master 9 (float entre 0 et 255)
+        self.add_method('/sub/9/level', 'f', self.sub9_level_cb)
+        # Master 10 (float entre 0 et 255)
+        self.add_method('/sub/10/level', 'f', self.sub10_level_cb)
+        # Master 1A (float entre 0 et 255)
+        self.add_method('/sub/11/level', 'f', self.sub11_level_cb)
+        # Master 12 (float entre 0 et 255)
+        self.add_method('/sub/12/level', 'f', self.sub12_level_cb)
 
-        #TODO
-        self.add_method('/pad/enter', None, self.fallback) # Enter
-        self.add_method('/pad/blackout', None, self.fallback) # Blackout (1 ou 0)
-        self.add_method('/pad/freeze', None, self.fallback) # Freeze (1 ou 0)
-        self.add_method('/pad/scene', None, self.fallback) # X1
+        # TODO :
+        self.add_method('/pad/enter', None, self.fallback)  # Enter
+        self.add_method('/pad/blackout', None, self.fallback)  # Blackout (1 ou 0)
+        self.add_method('/pad/freeze', None, self.fallback)  # Freeze (1 ou 0)
+        self.add_method('/pad/scene', None, self.fallback)  # X1
 
-        self.add_method('/seq/pause', None, self.fallback) # Pause
-        self.add_method('/seq/goback', None, self.fallback) # Go Back
-        self.add_method('/seq/fadeX1', None, self.fallback) # (float entre 0 et 255)
-        self.add_method('/seq/fadeX2', None, self.fallback) # (float entre 0 et 255)
-
-        self.add_method('/sub/2/level', None, self.fallback) # Master 2 (float entre 0 et 255)
+        self.add_method('/seq/pause', None, self.fallback)  # Pause
+        self.add_method('/seq/goback', None, self.fallback)  # Go Back
+        self.add_method('/seq/fadeX1', None, self.fallback)  # (float entre 0 et 255)
+        self.add_method('/seq/fadeX2', None, self.fallback)  # (float entre 0 et 255)
 
         # Register a fallback for unhandled messages
         self.add_method(None, None, self.fallback)
@@ -130,7 +146,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "1"
                 self.client.send('/pad/saisieText', self.window.keystring)
-                #print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def pad2_cb(self, path, args, types):
         """ Pad 2 """
@@ -138,7 +154,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "2"
                 self.client.send('/pad/saisieText', self.window.keystring)
-                #print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def pad3_cb(self, path, args, types):
         """ Pad 3 """
@@ -146,7 +162,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "3"
                 self.client.send('/pad/saisieText', self.window.keystring)
-                #print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def pad4_cb(self, path, args, types):
         """ Pad 4 """
@@ -154,7 +170,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "4"
                 self.client.send('/pad/saisieText', self.window.keystring)
-                #print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def pad5_cb(self, path, args, types):
         """ Pad 5 """
@@ -162,7 +178,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "5"
                 self.client.send('/pad/saisieText', self.window.keystring)
-                #print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def pad6_cb(self, path, args, types):
         """ Pad 6 """
@@ -170,7 +186,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "6"
                 self.client.send('/pad/saisieText', self.window.keystring)
-                #print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def pad7_cb(self, path, args, types):
         """ Pad 7 """
@@ -178,7 +194,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "7"
                 self.client.send('/pad/saisieText', self.window.keystring)
-                #print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def pad8_cb(self, path, args, types):
         """ Pad 8 """
@@ -186,7 +202,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "8"
                 self.client.send('/pad/saisieText', self.window.keystring)
-                #print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def pad9_cb(self, path, args, types):
         """ Pad 9 """
@@ -194,7 +210,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "9"
                 self.client.send('/pad/saisieText', self.window.keystring)
-                #print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def pad0_cb(self, path, args, types):
         """ Pad 0 """
@@ -202,7 +218,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "0"
                 self.client.send('/pad/saisieText', self.window.keystring)
-                #print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def paddot_cb(self, path, args, types):
         """ Pad . """
@@ -210,7 +226,7 @@ class OscServer(liblo.ServerThread):
             if a == 1:
                 self.window.keystring += "."
                 self.client.send('/pad/saisieText', self.window.keystring)
-                print("keystring :", self.window.keystring)
+                # print("keystring :", self.window.keystring)
 
     def padchannel_cb(self, path, args, types):
         """ Pad Channel """
@@ -286,7 +302,8 @@ class OscServer(liblo.ServerThread):
     def sub_launch_cb(self, path, args, types):
         """ Launch Sub page """
         for i in range(10):
-            self.client.send('/subStick/text', ('i', i+1), ('s', self.app.masters[i].text))
+            self.client.send('/subStick/text', ('i', i+1),
+                             ('s', self.app.masters[i].text))
 
     def sub_flash_cb(self, path, args, types):
         """ Flash Master """
