af1a443876b2a27359736d0b0e7f16266a92b410
mikacousin <mika.cousin@gmail.com>
2020-07-28 21:47:55 +0200

    Refactoring RegisterUniverse callback

--- src/ola_thread.py
+++ src/ola_thread.py
@@ -2,6 +2,7 @@
 
 import threading
 import array
+from functools import partial
 
 from ola import OlaClient
 from gi.repository import GLib
@@ -12,7 +13,7 @@ from olc.define import NB_UNIVERSES, App
 class OlaThread(threading.Thread):
     """Create OlaClient and receive universes updates
 
-    Attributes:
+    Args:
         universes: list of universes
     """
 
@@ -27,26 +28,29 @@ class OlaThread(threading.Thread):
             self.old_frame.append(array.array("B", [0] * 512))
 
     def run(self):
-        for i, univ in enumerate(self.universes):
-            func = getattr(self, "on_dmx_" + str(i), None)
-            self.ola_client.RegisterUniverse(univ, self.ola_client.REGISTER, func)
+        """Register universes"""
+        for univ in self.universes:
+            self.ola_client.RegisterUniverse(
+                univ, self.ola_client.REGISTER, partial(self.on_dmx, univ)
+            )
 
-    def on_dmx_0(self, dmxframe):
-        """Universe 0 updates.
+    def on_dmx(self, univ, dmxframe):
+        """Universe updates.
 
         Args:
-            dmxframe: array of 512 bytes with levels outputs
+            univ (int): universe
+            dmxframe (array): 512 bytes with levels outputs
         """
         # Find diff between old and new DMX frames
         diff = [
             (index, e1)
-            for index, (e1, e2) in enumerate(zip(dmxframe, self.old_frame[0]))
+            for index, (e1, e2) in enumerate(zip(dmxframe, self.old_frame[univ]))
             if e1 != e2
         ]
         # Loop on outputs with different level
         for output, level in diff:
-            channel = App().patch.outputs[0][output][0] - 1
-            App().dmx.frame[0][output] = level
+            channel = App().patch.outputs[univ][output][0] - 1
+            App().dmx.frame[univ][output] = level
             # New level
             App().window.channels[channel].level = level
             # Find next level
@@ -67,108 +71,8 @@ class OlaThread(threading.Thread):
             # Display new levels
             GLib.idle_add(App().window.channels[channel].queue_draw)
             if App().patch_outputs_tab:
-                GLib.idle_add(App().patch_outputs_tab.outputs[output].queue_draw)
+                GLib.idle_add(
+                    App().patch_outputs_tab.outputs[output + (univ * 512)].queue_draw
+                )
         # Save DMX frame for next call
-        self.old_frame[0] = dmxframe
-
-    def on_dmx_1(self, dmxframe):
-        """Universe 1 updates.
-
-        Args:
-            dmxframe: array of 512 bytes with levels outputs
-        """
-        diff = [
-            (index, e1)
-            for index, (e1, e2) in enumerate(zip(dmxframe, self.old_frame[1]))
-            if e1 != e2
-        ]
-        for output, level in diff:
-            channel = App().patch.outputs[1][output][0]
-            App().dmx.frame[1][output] = level
-            App().window.channels[channel - 1].level = level
-            if (
-                App().sequence.last > 1
-                and App().sequence.position < App().sequence.last - 1
-            ):
-                next_level = (
-                    App()
-                    .sequence.steps[App().sequence.position + 1]
-                    .cue.channels[channel - 1]
-                )
-            elif App().sequence.last:
-                next_level = App().sequence.steps[0].cue.channels[channel - 1]
-            else:
-                next_level = level
-            App().window.channels[channel - 1].next_level = next_level
-            GLib.idle_add(App().window.channels[channel - 1].queue_draw)
-            if App().patch_outputs_tab:
-                GLib.idle_add(App().patch_outputs_tab.outputs[output + 512].queue_draw)
-        self.old_frame[1] = dmxframe
-
-    def on_dmx_2(self, dmxframe):
-        """Universe 2 updates.
-
-        Args:
-            dmxframe: array of 512 bytes with levels outputs
-        """
-        diff = [
-            (index, e1)
-            for index, (e1, e2) in enumerate(zip(dmxframe, self.old_frame[2]))
-            if e1 != e2
-        ]
-        for output, level in diff:
-            channel = App().patch.outputs[2][output][0]
-            App().dmx.frame[2][output] = level
-            App().window.channels[channel - 1].level = level
-            if (
-                App().sequence.last > 1
-                and App().sequence.position < App().sequence.last - 1
-            ):
-                next_level = (
-                    App()
-                    .sequence.steps[App().sequence.position + 1]
-                    .cue.channels[channel - 1]
-                )
-            elif App().sequence.last:
-                next_level = App().sequence.steps[0].cue.channels[channel - 1]
-            else:
-                next_level = level
-            App().window.channels[channel - 1].next_level = next_level
-            GLib.idle_add(App().window.channels[channel - 1].queue_draw)
-            if App().patch_outputs_tab:
-                GLib.idle_add(App().patch_outputs_tab.outputs[output + 1024].queue_draw)
-        self.old_frame[2] = dmxframe
-
-    def on_dmx_3(self, dmxframe):
-        """Universe 3 updates.
-
-        Args:
-            dmxframe: array of 512 bytes with levels outputs
-        """
-        diff = [
-            (index, e1)
-            for index, (e1, e2) in enumerate(zip(dmxframe, self.old_frame[3]))
-            if e1 != e2
-        ]
-        for output, level in diff:
-            channel = App().patch.outputs[3][output][0]
-            App().dmx.frame[3][output] = level
-            App().window.channels[channel - 1].level = level
-            if (
-                App().sequence.last > 1
-                and App().sequence.position < App().sequence.last - 1
-            ):
-                next_level = (
-                    App()
-                    .sequence.steps[App().sequence.position + 1]
-                    .cue.channels[channel - 1]
-                )
-            elif App().sequence.last:
-                next_level = App().sequence.steps[0].cue.channels[channel - 1]
-            else:
-                next_level = level
-            App().window.channels[channel - 1].next_level = next_level
-            GLib.idle_add(App().window.channels[channel - 1].queue_draw)
-            if App().patch_outputs_tab:
-                GLib.idle_add(App().patch_outputs_tab.outputs[output + 1536].queue_draw)
-        self.old_frame[3] = dmxframe
+        self.old_frame[univ] = dmxframe
