0f56e9edc462ed1042d5cf49122e93f949e3fb94
Mika Cousin <mika.cousin@gmail.com>
2022-10-14 20:52:16 +0200

    Type hints

--- src/dmx.py
+++ src/dmx.py
@@ -14,12 +14,18 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 import array
 import threading
+from typing import List
 
 from olc.define import MAX_CHANNELS, NB_UNIVERSES, App
 
 
 class Dmx(threading.Thread):
     """Thread to send levels to Ola"""
+
+    grand_master: int
+    frame: list
+    sequence: array.array
+    user: array.array
 
     def __init__(self):
         threading.Thread.__init__(self)
@@ -30,12 +36,12 @@ class Dmx(threading.Thread):
         # User levels
         self.user = array.array("h", [-1] * MAX_CHANNELS)
 
-    def run(self):
+    def run(self) -> None:
         # les valeurs DMX echangées avec Ola
         for _ in range(NB_UNIVERSES):
             self.frame.append(array.array("B", [0] * 512))
 
-    def send(self):
+    def send(self) -> None:
         """Send DMX values to Ola"""
         univ = []  # To store universes changed
         for channel in range(MAX_CHANNELS):
@@ -89,12 +95,15 @@ class Dmx(threading.Thread):
 
 
 class PatchDmx:
-    """To store and manipulate DMX patch
+    """To store and manipulate DMX patch"""
 
-    Attributes:
-        channels (list): list of list of [output, universe]
-        outputs (list): list of universes who are list of [channel, level]
-    """
+    # channels: List of [output (1-512), universe (0-NB_UNIVERSES)] for each channel.
+    # For example, channel 5 could be patched on [1, 0] and [5, 1] i.e. output 1 of
+    # universe 1 and output 5 of universe 2
+    channels: List[List[List[int]]]
+    # outputs: List of [channel (1-MAX_CHANNELS), level (0-100)] for each output (1-512)
+    # of each universe (0-NB_UNIVERSES)
+    outputs: List[List[List[int]]]
 
     def __init__(self):
         # Default 1:1
@@ -103,29 +112,42 @@ class PatchDmx:
         self.channels = []
         for channel in range(MAX_CHANNELS):
             univ = int(channel / 512)
-            chan = channel - (512 * univ)
-            self.channels.append([[chan + 1, univ]])
+            output = (channel - (512 * univ)) + 1
+            self.channels.append([[output, univ]])
         # List of outputs
         self.outputs = []
         for universe in range(NB_UNIVERSES):
             self.outputs.append([])
-            for i in range(512):
-                channel = i + (512 * universe) + 1
+            for output in range(512):
+                channel = output + (512 * universe) + 1
                 if channel <= MAX_CHANNELS:
                     self.outputs[universe].append([channel, 100])
                 else:
                     self.outputs[universe].append([0, 100])
 
         # for channel in range(MAX_CHANNELS):
-        #     print("Channel", channel, "Output", self.channels[channel][0][0],
-        #     "Univers", self.channels[channel][0][1])
+        #     print(
+        #         "Channel",
+        #         channel,
+        #         "Output",
+        #         self.channels[channel][0][0],
+        #         "Univers",
+        #         self.channels[channel][0][1],
+        #     )
         # for universe in range(NB_UNIVERSES):
         #     for i in range(512):
-        #         print("Output", i + 1, "Univers", universe, "Channel",
-        #         self.outputs[universe][i][0], "Level",
-        #         self.outputs[universe][i][1])
+        #         print(
+        #             "Output",
+        #             i + 1,
+        #             "Univers",
+        #             universe,
+        #             "Channel",
+        #             self.outputs[universe][i][0],
+        #             "Level",
+        #             self.outputs[universe][i][1],
+        #         )
 
-    def patch_empty(self):
+    def patch_empty(self) -> None:
         """Set Dimmers patch to Zero"""
         for channel in range(MAX_CHANNELS):
             self.channels[channel] = [[0, 0]]
@@ -133,7 +155,7 @@ class PatchDmx:
             for output in range(512):
                 self.outputs[universe][output][0] = 0
 
-    def patch_1on1(self):
+    def patch_1on1(self) -> None:
         """Set patch 1:1"""
         for channel in range(MAX_CHANNELS):
             univ = int(channel / 512)
@@ -143,14 +165,16 @@ class PatchDmx:
             for output in range(512):
                 self.outputs[univ][output][0] = output + 1
 
-    def add_output(self, channel, output, univ, level=100):
+    def add_output(
+        self, channel: int, output: int, univ: int, level: int = 100
+    ) -> None:
         """Add an output to a channel
 
         Args:
             channel: Channel number
             output: Dimmer number
             univ: Universe number
-            level: Max level
+            level: Max level (0-100)
         """
         if self.channels[channel - 1] == [[0, 0]]:
             self.channels[channel - 1] = [[output, univ]]
