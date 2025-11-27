04146d3cb6fb9a94b8c1c22c5d9832630afe72f1
Mika Cousin <mika.cousin@gmail.com>
2025-11-27 17:52:01 +0100

    Fix Open File bug

--- src/files/parsed_data.py
+++ src/files/parsed_data.py
@@ -28,31 +28,22 @@ class ParsedData:
 
     patch: dict[int, list]
     sequence: dict
+    data: dict
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.data = {
-            "console": {
-                "console": "",
-                "manufacturer": ""
-            },
+            "console": {"console": "", "manufacturer": ""},
             "curves": {},
             "patch": {},
-            "sequences": {
-                1: {
-                    "label": "",
-                    "mode": "normal",
-                    "steps": {},
-                    "cues": {}
-                }
-            },
+            "sequences": {1: {"label": "", "mode": "normal", "steps": {}, "cues": {}}},
             "groups": {},
             "independents": {},
             "cues": {},
             "faders": {},
-            "midi": {}
+            "midi": {},
         }
 
-    def clean(self):
+    def clean(self) -> None:
         """Remove useless data"""
         # Remove Sequences without cue
         keys = []
@@ -63,6 +54,8 @@ class ParsedData:
             self.data["sequences"].pop(key)
         # Transform fader of channels in fader with group
         index = 500.0
+        if not self.data["faders"]:
+            return
         for values in self.data["faders"].values():
             if values.get("type") == FaderType.CHANNELS:
                 while True:
@@ -72,7 +65,7 @@ class ParsedData:
                         break
                 self.data["groups"][index] = {
                     "channels": values.get("contents"),
-                    "label": str(index)
+                    "label": str(index),
                 }
                 values["type"] = FaderType.GROUP
                 values["contents"] = index
@@ -113,7 +106,8 @@ class ParsedData:
                 points = values["points"]
                 for point in points:
                     App().lightshow.curves.curves[curve_nb].add_point(
-                        point[0], point[1])
+                        point[0], point[1]
+                    )
             label = values.get("label")
             if label:
                 curve.name = label
@@ -132,7 +126,6 @@ class ParsedData:
         for group_number, values in self.data["groups"].items():
             channels = values.get("channels")
             label = values.get("label")
-            group = None
             found = False
             for group in App().lightshow.groups:
                 if group_number == group.index:
@@ -152,14 +145,18 @@ class ParsedData:
         Args:
             actions: Imported action
         """
+        if not self.data["faders"]:
+            return
         for values in self.data["faders"].values():
             page = values.get("page")
             number = values.get("number")
             fader_type = values.get("type")
             contents = values.get("contents")
             # Don't create fader for non imported elements
-            if fader_type == FaderType.SEQUENCE and actions["sequences"][
-                    contents] is Action.IGNORE:
+            if (
+                fader_type == FaderType.SEQUENCE
+                and actions["sequences"][contents] is Action.IGNORE
+            ):
                 continue
             if fader_type == FaderType.GROUP and actions["groups"] is Action.IGNORE:
                 continue
@@ -171,7 +168,6 @@ class ParsedData:
         for inde_number, values in self.data["independents"].items():
             channels = values.get("channels")
             label = values.get("label")
-            inde = None
             found = False
             for inde in App().lightshow.independents.independents:
                 if inde_number == inde.number:
@@ -188,7 +184,6 @@ class ParsedData:
             channels = values.get("channels")
             label = values.get("label")
             found = False
-            cue = None
             for cue in App().lightshow.cues:
                 if cue.memory == preset_number:
                     found = True
@@ -216,10 +211,10 @@ class ParsedData:
         for step, values in self.data["sequences"][sequence]["steps"].items():
             cue_number = values.get("cue")
             cue_channels = self.data["sequences"][sequence]["cues"][cue_number][
-                "channels"]
+                "channels"
+            ]
             cue_text = self.data["sequences"][sequence]["cues"][cue_number]["label"]
             found = False
-            cue = None
             for cue in App().lightshow.cues:
                 if cue.memory == cue_number:
                     found = True
@@ -235,18 +230,21 @@ class ParsedData:
             channel_time = {}
             if values.get("channel_time"):
                 for channel, times in values.get("channel_time").items():
-                    channel_time[channel] = ChannelTime(times.get("delay", 0.0),
-                                                        times.get("time", 0.0))
+                    channel_time[channel] = ChannelTime(
+                        times.get("delay", 0.0), times.get("time", 0.0)
+                    )
             # Create new step
-            step = Step(sequence,
-                        cue,
-                        time_in=values.get("time_in"),
-                        time_out=values.get("time_out"),
-                        delay_in=values.get("delay_in", 0.0),
-                        delay_out=values.get("delay_out", 0.0),
-                        wait=values.get("wait", 0.0),
-                        channel_time=channel_time,
-                        text=values.get("label", ""))
+            step = Step(
+                sequence,
+                cue,
+                time_in=values.get("time_in"),
+                time_out=values.get("time_out"),
+                delay_in=values.get("delay_in", 0.0),
+                delay_out=values.get("delay_out", 0.0),
+                wait=values.get("wait", 0.0),
+                channel_time=channel_time,
+                text=values.get("label", ""),
+            )
             App().lightshow.main_playback.add_step(step)
 
     def _merge_main_playback(self, sequence: int) -> None:
@@ -254,7 +252,8 @@ class ParsedData:
         for step, values in self.data["sequences"][sequence]["steps"].items():
             cue_number = values.get("cue")
             cue_channels = self.data["sequences"][sequence]["cues"][cue_number][
-                "channels"]
+                "channels"
+            ]
             cue_text = self.data["sequences"][sequence]["cues"][cue_number]["label"]
             found, step_nb = App().lightshow.main_playback.get_step(cue_number)
             if found:
@@ -262,25 +261,29 @@ class ParsedData:
                 cue = App().lightshow.main_playback.steps[step_nb - 1].cue
                 cue.channels = cue_channels
                 cue.text = cue_text
-                App().lightshow.main_playback.steps[step_nb -
-                                                    1].text = values.get("label")
+                App().lightshow.main_playback.steps[step_nb - 1].text = values.get(
+                    "label"
+                )
             else:
                 cue = Cue(0, cue_number, cue_channels, text=cue_text)
                 self._insert_cue(cue)
                 channel_time = {}
                 if values.get("channel_time"):
                     for channel, times in values.get("channel_time").items():
-                        channel_time[channel] = ChannelTime(times.get("delay", 0.0),
-                                                            times.get("time", 0.0))
-                step = Step(sequence,
-                            cue,
-                            time_in=values.get("time_in"),
-                            time_out=values.get("time_out"),
-                            delay_in=values.get("delay_in", 0.0),
-                            delay_out=values.get("delay_out", 0.0),
-                            wait=values.get("wait", 0.0),
-                            channel_time=channel_time,
-                            text=values.get("label", ""))
+                        channel_time[channel] = ChannelTime(
+                            times.get("delay", 0.0), times.get("time", 0.0)
+                        )
+                step = Step(
+                    sequence,
+                    cue,
+                    time_in=values.get("time_in"),
+                    time_out=values.get("time_out"),
+                    delay_in=values.get("delay_in", 0.0),
+                    delay_out=values.get("delay_out", 0.0),
+                    wait=values.get("wait", 0.0),
+                    channel_time=channel_time,
+                    text=values.get("label", ""),
+                )
                 App().lightshow.main_playback.insert_step(step_nb, step)
 
     def import_chaser(self, sequence: int, action: Action) -> None:
@@ -292,63 +295,68 @@ class ParsedData:
         """
         label = self.data["sequences"][sequence]["label"]
         chaser = None
-        index = None
-        for index, chsr in enumerate(App().lightshow.chasers):
+        for _index, chsr in enumerate(App().lightshow.chasers):
             if chsr.index == sequence:
                 chaser = chsr
                 break
         if not chaser:
             App().lightshow.chasers.append(
-                Sequence(sequence, type_seq="Chaser", text=label))
-            index = -1
-            del App().lightshow.chasers[index].steps[1:]
+                Sequence(sequence, type_seq="Chaser", text=label)
+            )
+            _index = -1
+            del App().lightshow.chasers[_index].steps[1:]
         else:
-            App().lightshow.chasers[index].text = label
+            App().lightshow.chasers[_index].text = label
         if action is Action.REPLACE:
-            self._replace_chaser(sequence, index)
+            self._replace_chaser(sequence, _index)
         elif action is Action.MERGE:
-            self._merge_chaser(sequence, index)
+            self._merge_chaser(sequence, _index)
 
     def _merge_chaser(self, sequence: int, index: int) -> None:
         for step, values in self.data["sequences"][sequence]["steps"].items():
             cue_number = values.get("cue")
             cue_channels = self.data["sequences"][sequence]["cues"][cue_number][
-                "channels"]
+                "channels"
+            ]
             cue_text = self.data["sequences"][sequence]["cues"][cue_number]["label"]
             found, step_nb = App().lightshow.chasers[index].get_step(cue_number)
             if found:
                 cue = App().lightshow.chasers[index].steps[step_nb - 1].cue
                 cue.channels = cue_channels
                 cue.text = cue_text
-                App().lightshow.chasers[index].steps[step_nb -
-                                                     1].text = values.get("label")
+                App().lightshow.chasers[index].steps[step_nb - 1].text = values.get(
+                    "label"
+                )
             else:
                 cue = Cue(sequence, cue_number, cue_channels, text=cue_text)
                 App().lightshow.chasers[index].cues.add(cue)
                 channel_time = {}
                 if values.get("channel_time"):
                     for channel, times in values.get("channel_time").items():
-                        channel_time[channel] = ChannelTime(times.get("delay", 0.0),
-                                                            times.get("time", 0.0))
-                step = Step(sequence,
-                            cue,
-                            time_in=values.get("time_in"),
-                            time_out=values.get("time_out"),
-                            delay_in=values.get("delay_in", 0.0),
-                            delay_out=values.get("delay_out", 0.0),
-                            wait=values.get("wait", 0.0),
-                            channel_time=channel_time,
-                            text=values.get("label", ""))
+                        channel_time[channel] = ChannelTime(
+                            times.get("delay", 0.0), times.get("time", 0.0)
+                        )
+                step = Step(
+                    sequence,
+                    cue,
+                    time_in=values.get("time_in"),
+                    time_out=values.get("time_out"),
+                    delay_in=values.get("delay_in", 0.0),
+                    delay_out=values.get("delay_out", 0.0),
+                    wait=values.get("wait", 0.0),
+                    channel_time=channel_time,
+                    text=values.get("label", ""),
+                )
                 App().lightshow.chasers[index].insert_step(step_nb, step)
 
     def _replace_chaser(self, sequence: int, index: int) -> None:
         for step, values in self.data["sequences"][sequence]["steps"].items():
             cue_number = values.get("cue")
             cue_channels = self.data["sequences"][sequence]["cues"][cue_number][
-                "channels"]
+                "channels"
+            ]
             cue_text = self.data["sequences"][sequence]["cues"][cue_number]["label"]
             found = False
-            cue = None
             for cue in App().lightshow.chasers[index].cues:
                 if cue.memory == cue_number:
                     found = True
@@ -364,18 +372,21 @@ class ParsedData:
             channel_time = {}
             if values.get("channel_time"):
                 for channel, times in values.get("channel_time").items():
-                    channel_time[channel] = ChannelTime(times.get("delay", 0.0),
-                                                        times.get("time", 0.0))
+                    channel_time[channel] = ChannelTime(
+                        times.get("delay", 0.0), times.get("time", 0.0)
+                    )
             # Create new step
-            step = Step(sequence,
-                        cue,
-                        time_in=values.get("time_in"),
-                        time_out=values.get("time_out"),
-                        delay_in=values.get("delay_in", 0.0),
-                        delay_out=values.get("delay_out", 0.0),
-                        wait=values.get("wait", 0.0),
-                        channel_time=channel_time,
-                        text=values.get("label", ""))
+            step = Step(
+                sequence,
+                cue,
+                time_in=values.get("time_in"),
+                time_out=values.get("time_out"),
+                delay_in=values.get("delay_in", 0.0),
+                delay_out=values.get("delay_out", 0.0),
+                wait=values.get("wait", 0.0),
+                channel_time=channel_time,
+                text=values.get("label", ""),
+            )
             App().lightshow.chasers[index].add_step(step)
 
     def _insert_cue(self, cue: Cue) -> None:
@@ -387,14 +398,14 @@ class ParsedData:
         cue_number = cue.memory
         # Find cue position
         found = False
-        index = 0
-        for index, cue_seq in enumerate(App().lightshow.cues):
+        _index = 0
+        for _index, cue_seq in enumerate(App().lightshow.cues):
             if cue_seq.memory > cue_number:
                 found = True
                 break
         if not found:
             # Cue is at the end
-            index += 1
+            _index += 1
         # Insert cue
-        App().lightshow.cues.insert(index, cue)
+        App().lightshow.cues.insert(_index, cue)
         App().lightshow.main_playback.update_channels()
